866ca0c3724090840fd45713e68e9c51008ac952
nomandhoni-cs <nomandhoni@yahoo.com>
2022-10-27 18:48:44 +0600

    Used Bootstrap icon and F.Awesome

--- src/Components/Bucket/Bucket.js
+++ src/Components/Bucket/Bucket.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import './Bucket.css'
+import './Bucket.css';
+import { BsFillBucketFill } from "react-icons/bs";
+
 const Bucket = () => {
     return (
         <div>
-            
+            <h3><BsFillBucketFill /> Bucketlist</h3>
         </div>
     );
 };
