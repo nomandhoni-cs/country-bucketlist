3cb1949fc287427c0e52d03f7811eaf28789c849
nomandhoni-cs <nomandhoni@yahoo.com>
2022-10-27 17:32:23 +0600

    Added bootstrap and api

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Bucket from '../Bucket/Bucket';
 import Country from '../Country/Country';
 import './Body.css'
 const Body = () => {
+    const [country, setCountry] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(countries =>{
+        const firstTwentyCountries = countries.slice(0, 20);
+        setCountry(firstTwentyCountries);
+        // console.log(firstTwentyCountries);
+        })
+    }, []);
+
+
     return (
         <div className='container'>
             <div className="row">
                 <div className="col-xl-8">
                     <div className="country-area">
-                        <Country></Country>
+                        {
+                            country.map(desh => {return <Country country={desh.name}></Country>})
+                        }
                     </div>
                 </div>
                 <div className="col-xl-4">
--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import './Country.css'
-const Country = () => {
+const Country = (props) => {
+
     return (
         <div className='country-container'>
             <div className="single-country">
                 <div className="country-img">
-
+                    <img src="https://flagcdn.com/w320/mn.png" alt="" />
                 </div>
+                {
+                        console.log(props.country)
+                }
                 <div className="country-name">Bangladesh</div>
                 <div className="bucket-btn">+</div>
             </div>
             <div className="single-country">
                 <div className="country-img">
-
-                </div>
-                <div className="country-name">Bangladesh</div>
-                <div className="bucket-btn">+</div>
-            </div>
-            <div className="single-country">
-                <div className="country-img">
-
+                <img src="https://flagcdn.com/w320/bd.png" alt="" />
                 </div>
                 <div className="country-name">Bangladesh</div>
                 <div className="bucket-btn">+</div>
