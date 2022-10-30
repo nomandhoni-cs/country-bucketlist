9ec99c26fda404c868711e9d44e21655e1ac808f
nomandhoni-cs <nomandhoni@yahoo.com>
2022-10-30 13:07:26 +0600

    used lazy load in image slider

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel'
 import './Carousel.css'
+import { LazyLoadImage } from "react-lazy-load-image-component";
 const DarkVariantExample = () => {
     return (
         <div className='container'>
     <Carousel variant="dark">
       <Carousel.Item>
-        <img
+        <LazyLoadImage
           className="d-block w-100 carousel-img"
           src="https://www.traveltourxp.com/wp-content/uploads/2017/01/Attractions-In-Barbados.jpg"
           alt="First slide"
@@ -16,17 +17,13 @@ const DarkVariantExample = () => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100 carousel-img"
-          src="https://cdn.thecrazytourist.com/wp-content/uploads/2016/09/Amboseli-National-Park.jpg"
-          alt="Second slide"
-        />
+          <LazyLoadImage className="d-block w-100 carousel-img" src="https://cdn.thecrazytourist.com/wp-content/uploads/2016/09/Amboseli-National-Park.jpg" alt="Second slide" />
         <Carousel.Caption>
           <h4 className="carousel-caption">Amboseli National Park Kenya</h4>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
+        <LazyLoadImage
           className="d-block w-100 carousel-img"
           src="https://cdn.thecrazytourist.com/wp-content/uploads/2017/03/El-Djem.jpg"
           alt="Third slide"
